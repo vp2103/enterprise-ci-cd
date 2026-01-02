77b63f7cef466c312ed36814d654a4190ec98ce4
varun <varunpeddi2000@gmail.com>
2026-01-02 17:48:41 +0530

    Add CD stage with Docker deployment

--- app/app.py
+++ app/app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "CI pipeline is working!"
+def hello():
+    return "Enterprise CI/CD Pipeline Working!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
